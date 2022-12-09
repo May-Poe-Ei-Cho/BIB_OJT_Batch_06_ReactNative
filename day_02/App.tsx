99a9f13ca342e587a74d6fed5fb581cadc1d6112
May-Poe-Ei-Cho <maypoeeicho72@gmail.com>
2022-12-10 00:09:34 +0630

    second commit

--- day_02/App.tsx
+++ day_02/App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput,Alert } from 'react-native';
 
 export default function App() {
+  const [number, setNumber] = useState("100000");
+  const [usd, setUSD] = useState('');
+  const [baht, setbaht] = useState('');
+  const [yuan, setyuan ] = useState('') ;
+
+  const clickMe = () => {
+    setUSD ( "USD  = " + number * 0.00048);
+    setbaht( "baht = " + number * 0.017);
+    setyuan( "Yuan = " + number * 0.0033);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+      <Text style={styles.title}>MMK</Text>
+      <TextInput onChangeText={(num) => setNumber(num)} style={styles.input} keyboardType="numeric" />
+      <Button style={styles.button} title='Convert' onPress={clickMe}/>
+      <View style={styles.content}>
+        <Text style={styles.title}> {usd} </Text>
+        <Text style={styles.title}> {baht} </Text>
+        <Text style={styles.title}> {yuan} </Text>
+      </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  appbar: {
+    marginTop: 70,
+    marginLeft: 10,
+    marginBottom: 10,
+    marginRight: 10,
+  },
+  content: {
+    marginTop: 40,
+ 
+  },
+  button: {
+    marginBottom: 40,
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 2,
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 6,
+  },
+  title: {
+    fontSize: 19,
+  }
 });
+
